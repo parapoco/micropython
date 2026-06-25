4572e4a58b47a6676ff6d882e3e92eed8f74affc
parapoco <hoteifoodssk2@gmail.com>
2026-06-26 00:21:34 +0900

    Register custom C module 'mdx' in MicroPython
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/micropython.cmake
+++ ports/rp2/micropython.cmake
@@ -3,3 +3,8 @@ usermod_extension_bind_factories(mdx)
 target_sources(usermod INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
 )
+# 自作CモジュールをMicroPythonに登録
+micropython_add_user_cmodule(
+    MODULE mdx
+    SRC ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
+)
