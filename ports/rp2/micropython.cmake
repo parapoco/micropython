5052a44de3ed94d2a1c24dcefdfa59a36d88f000
parapoco <hoteifoodssk2@gmail.com>
2026-06-26 00:41:55 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/micropython.cmake
+++ ports/rp2/micropython.cmake
@@ -3,8 +3,13 @@ usermod_extension_bind_factories(mdx)
 target_sources(usermod INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
 )
-# 自作CモジュールをMicroPythonに登録
+# 自作Cモジュール（usermod）として正式登録
 micropython_add_user_cmodule(
     MODULE mdx
     SRC ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
+)
+
+# モジュールを常に有効化
+target_compile_definitions(usermod INTERFACE
+    MODULE_MDX_ENABLED=1
 )
