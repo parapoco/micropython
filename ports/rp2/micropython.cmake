4ddc89a6a50cc7c946b549087c1f88e6c60309b5
parapoco <hoteifoodssk2@gmail.com>
2026-06-26 14:06:13 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/micropython.cmake
+++ ports/rp2/micropython.cmake
@@ -1,1 +1,10 @@
+# ====================================================================
+# MicroPython ユーザーCモジュール 登録スクリプト
+# ====================================================================
 
+# CモジュールのソースコードとヘッダーをMicroPythonのビルドシステムに登録します
+interface_userspace_cmodules_register_headers_and_sources(
+    ${CMAKE_CURRENT_LIST_DIR}
+    MODULE_NAME mymodule
+    SOURCES mymodule.c
+)
