1f78589be9c6c646019730a4f91d07812aac976a
parapoco <hoteifoodssk2@gmail.com>
2026-06-27 00:15:03 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -1,4 +1,11 @@
-micropython_add_user_cmodule(
-    MODULE mdx
-    SRC ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
+add_library(usermod_mdx INTERFACE)
+
+target_sources(usermod_mdx INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
 )
+
+target_include_directories(usermod_mdx INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+target_link_libraries(usermod INTERFACE usermod_mdx)
