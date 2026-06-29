85597e333a60c7906ed3cb3c1201edc900affa8c
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 05:29:38 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -18,14 +18,14 @@ set(MDX_SOURCES
 
 target_sources(usermod_mdx INTERFACE ${MDX_SOURCES})
 
-# ⭕【ここをさらに強化】C++ファイルに 符号比較の警告を消すフラグ（-Wno-sign-compare）を追加
+# C言語とC++で、渡すフラグをきっちり分ける処理
 foreach(src ${MDX_SOURCES})
     if(${src} MATCHES "\\.cpp$")
         # C++ファイル用のお守りフルセット
         set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-conversion-null -Wno-sign-compare")
     elseif(${src} MATCHES "\\.c$")
-        # C言語ファイル用
-        set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable")
+        # ⭕【ここを最終強化】古いMAMEのCコード（ym2151.c）を無傷で突破するコンパイルフラグ
+        set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-implicit-function-declaration -Wno-builtin-declaration-mismatch -Wno-implicit-int -include math.h -include string.h")
     endif()
 endforeach()
 
