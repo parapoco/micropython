27e1a1be12d31d920a2c2e64e9e023a9599b5386
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 04:05:10 +0900

    Refactor micropython.cmake for clarity and corrections
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -1,14 +1,13 @@
 # ports/rp2/mdx/micropython.cmake
 
-# モジュール（usermod_mdx）の定義
 add_library(usermod_mdx INTERFACE)
 
-# ⭕ 元のCMakeLists.txtに書かれていた「本物のファイル一覧」に修正！
+# 元のCMakeLists.txtに書かれていた「本物のファイル一覧」
 target_sources(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/downsample/downsample.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmgen.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmtimer.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmtimer.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/opm.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/so.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/opm_delegate.cpp
@@ -17,15 +16,10 @@ target_sources(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/mame/ym2151.c
 )
 
-# インクルードパス（ヘッダーファイルを探す場所）にこのフォルダを追加
+# インクルードパスの設定
 target_include_directories(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-# Pico SDK の I2S オーディオライブラリをリンクする
-target_link_libraries(usermod_mdx INTERFACE
-    pico_audio_i2s
-)
-
-# このモジュールをMicroPython本体（usermod）へと結合する
+# MicroPython本体へ結合
 target_link_libraries(usermod INTERFACE usermod_mdx)
