9b8e0d10e684676b107cd5256cfac4b97d534564
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 04:58:23 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -2,8 +2,8 @@
 
 add_library(usermod_mdx INTERFACE)
 
-# ⭕ 今度こそフォルダ階層を完全に正しく直しました！
-target_sources(usermod_mdx INTERFACE
+# ソースファイル一覧を変数にまとめる
+set(MDX_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/downsample/downsample.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmgen.cpp
@@ -15,6 +15,15 @@ target_sources(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/x68pcm8.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/mame/ym2151.c
 )
+
+target_sources(usermod_mdx INTERFACE ${MDX_SOURCES})
+
+# ⭕【ここを追加】gamdxの古いコード特有の「未使用警告」をエラーにしないおまじない
+foreach(src ${MDX_SOURCES})
+    if(${src} MATCHES "\\.(c|cpp)$")
+        set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-error=unused-variable -Wno-error=unused-function")
+    endif()
+endforeach()
 
 # インクルードパスの設定
 target_include_directories(usermod_mdx INTERFACE
