202e30b5df21e64478bf83045d645aef45b9c41e
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 03:39:43 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -1,16 +1,20 @@
 # ports/rp2/mdx/micropython.cmake
 
-# ⭕ 古い呪文を廃止し、モジュール（usermod_mdx）を新方式で定義
+# モジュール（usermod_mdx）の定義
 add_library(usermod_mdx INTERFACE)
 
-# コンパイル対象のCファイルを指定
+# ⭕ 元のCMakeLists.txtに書かれていた「本物のファイル一覧」に修正！
 target_sources(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
-    
-    # 🌟 gamdx内のコンパイルに必要な3つのファイルを指定
-    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/mxdrvg.c
-    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/mxdrv_v.c
-    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/mxdrv_f.c
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/downsample/downsample.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmgen.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmtimer.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/opm.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/so.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/opm_delegate.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/pcm8.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/x68pcm8.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mame/ym2151.c
 )
 
 # インクルードパス（ヘッダーファイルを探す場所）にこのフォルダを追加
@@ -23,5 +27,5 @@ target_link_libraries(usermod_mdx INTERFACE
     pico_audio_i2s
 )
 
-# ⭕ 最後にこのモジュールをMicroPython本体（usermod）へと結合する
+# このモジュールをMicroPython本体（usermod）へと結合する
 target_link_libraries(usermod INTERFACE usermod_mdx)
