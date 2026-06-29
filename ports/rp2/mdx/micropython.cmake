935d8723c4b7661d6e96604e743326dddf95b5b8
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 01:32:25 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -1,11 +1,24 @@
-add_library(usermod_mdx INTERFACE)
+# ports/rp2/mdx/micropython.cmake
 
+# モジュール（mdx）の定義
+micropython_add_user_cmodule(mdx)
+
+# コンパイル対象のCファイルを指定
 target_sources(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
+    
+    # 🌟 gamdx内のコンパイルに必要な3つのファイルを指定
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/mxdrvg.c
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/mxdrv_v.c
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/mxdrv_f.c
 )
 
+# インクルードパス（ヘッダーファイルを探す場所）にこのフォルダを追加
 target_include_directories(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-target_link_libraries(usermod INTERFACE usermod_mdx)
+# Pico SDK の I2S オーディオライブラリをリンクする
+target_link_libraries(usermod_mdx INTERFACE
+    pico_audio_i2s
+)
