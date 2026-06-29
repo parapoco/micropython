c86b7674ed62c7813fd68b32ce66bd649baf95c9
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 06:26:02 +0900

    Modify compile flags and adjust library linking
    Updated compile flags for C++ files and removed conflicting library link.
    
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -2,7 +2,6 @@
 
 add_library(usermod_mdx INTERFACE)
 
-# ソースファイル一覧を変数にまとめる
 set(MDX_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/downsample/downsample.cpp
@@ -18,21 +17,22 @@ set(MDX_SOURCES
 
 target_sources(usermod_mdx INTERFACE ${MDX_SOURCES})
 
-# C言語とC++で、渡すフラグをきっちり分ける処理
+# 必要なマクロ定義のみ継承
+target_compile_definitions(usermod_mdx INTERFACE
+    MDX_VOLUME=40
+)
+
 foreach(src ${MDX_SOURCES})
     if(${src} MATCHES "\\.cpp$")
-        # C++ファイル用のお守りフルセット
-        set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-conversion-null -Wno-sign-compare")
+        set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-conversion-null -Wno-sign-compare -fno-exceptions -fno-rtti")
     elseif(${src} MATCHES "\\.c$")
-        # ⭕【ここを最終強化】古いMAMEのCコード（ym2151.c）を無傷で突破するコンパイルフラグ
         set_source_files_properties(${src} PROPERTIES COMPILE_FLAGS "-Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable -Wno-implicit-function-declaration -Wno-builtin-declaration-mismatch -Wno-implicit-int -include math.h -include string.h")
     endif()
 endforeach()
 
-# インクルードパスの設定
 target_include_directories(usermod_mdx INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-# MicroPython本体へ結合
+# ⭕ pico_audio_i2s のリンクを削除（MicroPython本体と喧嘩させないため）
 target_link_libraries(usermod INTERFACE usermod_mdx)
