73f911578cd0639f6946b2b250a3f1901c1b77eb
parapoco <hoteifoodssk2@gmail.com>
2026-06-30 10:54:10 +0900

    Update micropython.cmake
    Signed-off-by: parapoco <hoteifoodssk2@gmail.com>

--- ports/rp2/mdx/micropython.cmake
+++ ports/rp2/mdx/micropython.cmake
@@ -4,14 +4,14 @@ add_library(usermod_mdx INTERFACE)
 
 set(MDX_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/modmdx.c
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/downsample/downsample.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmgen.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmtimer.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/opm.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/so.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/opm_delegate.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/pcm8.cpp
-    #${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/x68pcm8.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/downsample/downsample.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmgen.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/fmtimer.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/fmgen/opm.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/so.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/mxdrvg/opm_delegate.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/pcm8.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/gamdx/pcm8/x68pcm8.cpp
     ${CMAKE_CURRENT_LIST_DIR}/gamdx/mame/ym2151.c
 )
 
